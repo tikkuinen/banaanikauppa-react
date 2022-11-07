fa96c2397873c0a97ce96d0bba17b2e4a438eea8
tikkuinen <titunen@gmail.com>
2022-11-07 19:11:36 +0200

    kommentointia

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 export default function Cart() {
   return (
     <Link to="">
-      <FontAwesomeIcon icon={faCartShopping} />
-      <span style={{color: '#fff'}}></span>
+      {/* <FontAwesomeIcon icon={faCartShopping} />
+      <span style={{color: '#fff'}}></span> */}
     </Link>
   )
 }
