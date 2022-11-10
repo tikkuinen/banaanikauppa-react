b4fc5a7945490e17f588b0d5ec385633ea668167
tikkuinen <titunen@gmail.com>
2022-11-10 16:33:01 +0200

    tuotehakua aloitettu, ei toimi

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Products from './pages/Products';
 
+
 // tässä pitää olla se omalla koneella oleva polku php-kansioon
 const URL = 'http://localhost/banaanikauppa/';
 
 function App() {
+  // ostoskori määritellään täällä tilamuuttujana, jotta sitä voidaan käyttää muissa komponenteissa
+  const [cart, setCart] = useState([]);
+
+  // ja tallennetaan ostoskoria jotenkin
+  function addToCart(product) {
+    const newCart = [...cart.product];
+    setCart(newCart);
+    localStorage.setItem('cart',JSON.stringify(newCart));
+  }
+
   return (
     <>
       <Navbar url={URL}/>
       <div className='container'>
         <Routes>
             <Route path='/' element= {<Home />}/>
-            <Route path='/products' element= {<Products />}/>
+            <Route path='/products' element= {<Products url={URL}/>}/>
             <Route path='/about' element= {<About />}/>
         </Routes>
       </div>
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,21 @@
+import axios from 'axios';
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    axios.get(url + 'products/getproducts.php/')
+      .then((response) => {
+        const json = response.data;
+        console.log(json);
+      }).catch(error => {
+        alert(error.response === undefined ? error : error.response.data.error);
+      })
+  }, [])
+  
   return (
     <div>Products</div>
   )
