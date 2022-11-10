8572045a91f883b5159c10e73c9fb3d3e3e192e2
tikkuinen <titunen@gmail.com>
2022-11-10 15:39:43 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import React,{useState,useEffect} from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
