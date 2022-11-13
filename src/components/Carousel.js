8d6b8a3784348cdd7a4bd07af107f6735c0a6dcb
tikkuinen <titunen@gmail.com>
2022-11-13 13:33:40 +0200

    karuselli toimii

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,22 +2,24 @@ import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
-export default function Carousel(){
-const handleDragStart = (e) => e.preventDefault();
+export default function Carousel() {
+  const handleDragStart = (e) => e.preventDefault();
 
-const items = [
-  <img src="../images/kuva1.jpg" onDragStart={handleDragStart} role="presentation" />,
-  <img src="../images/kuva2.jpg" onDragStart={handleDragStart} role="presentation" />,
-  <img src="../images/kuva3.jpg" onDragStart={handleDragStart} role="presentation" />,
+  const items = [
+    <div className='item'>
+      <img src={require("../images/kuva1.jpg")} alt="kuva1" onDragStart={handleDragStart} role="presentation" />
+    </div>,
+    <div className='item'>
+      <img src={require("../images/kuva2.jpg")} alt="kuva2" onDragStart={handleDragStart} role="presentation" />
+    </div>,
+    <div className='item'>
+      <img src={require("../images/kuva3.jpg")} alt="kuva3" onDragStart={handleDragStart} role="presentation" />
+    </div>
+  ];
 
-];
-
-console.log(items.length);
   return (
-    const Gallery = () => {
-    return (
-      <AliceCarousel mouseTracking items={items} />
-    );
-  })
-
+      <div className='App'>
+        <AliceCarousel mouseTracking items={items} />
+      </div>
+  );
 }
