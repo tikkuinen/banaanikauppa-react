7035e052dbb1faaced56f86518f1346e391d324c
tikkuinen <titunen@gmail.com>
2022-12-03 17:55:10 +0200

    karuselli toimii tuoteryhmällä

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,49 +1,82 @@
 import {React, useEffect, useState} from 'react';
+import { Link } from 'react-router-dom'
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import axios from 'axios';
 
 export default function Carousel({url}) {
   const [product, setProduct] = useState([]);
+  const [image, setImage] = useState("");
 
-  //hakee taulukkona product-taulusta kaikki
-  // useEffect(() => {
-  //   axios.get(url + 'products/getrandom.php/')
-  //     .then((response) => {
-  //       const json = response.data;
-  //       setProduct(json);
-  //       //console.log(json);
-  //     }).catch(error => {
-  //       alert(error.response === undefined ? error : error.response.data.error);
-  //     })
-  // }, [])
-  
-  // jossain pitäisi arpoa sieltä taulukosta joku rivi
-  const randomizer = () => {
-    let rand = 0;
-    // tähän se satunnaisluku
-  }
+  // tässä random luku, menee tohon koodiin
+
+  // hakee taulukkona product-taulusta yhden, mut pitäiskö olla random kategoria?
+  useEffect(() => {
+    axios.get(url + 'products/getproducts.php/3')
+      .then((response) => {
+        const json = response.data;
+        setProduct(json.products);
+        console.log(json.products);
+        //const amount = json.products.length;
+        // ottaa 0 - pituus eli maksmäärä
+        //setImage(json.products[Math.floor(Math.random() * (amount - 0) ) + 0].image);
+      }).catch(error => {
+        alert(error.response === undefined ? error : error.response.data.error);
+      })
+  }, [])
   
   const handleDragStart = (e) => e.preventDefault();
 
-  const items = [
+  const responsive = {
+    0: {
+        items: 1
+    },
+    568: {
+        items: 2
+    },
+    1024: {
+        items: 3
+    },
+  };
+
+  const items = product.map(item => (
     <div className='item'>
-      <img src={require("../images/kuva1.jpg")} alt="kuva1" onDragStart={handleDragStart} role="presentation" />
-    </div>,
-    // <div className='item'>
-    //   <img src={require(json[randomizer()])} alt="kuva1" onDragStart={handleDragStart} role="presentation" />
-    // </div>,
-    <div className='item'>
-      <img src={require("../images/kuva2.jpg")} alt="kuva2" onDragStart={handleDragStart} role="presentation" />
-    </div>,
-    <div className='item'>
-      <img src={require("../images/kuva3.jpg")} alt="kuva3" onDragStart={handleDragStart} role="presentation" />
+      <Link to={'../product/' + item.product_id}>
+      <img src={url + 'images/' + item.image} alt="kuva" onDragStart={handleDragStart} role="presentation" />
+      </Link>
     </div>
-  ];
+  ));
+
+
+  // const items = [
+  //   <div className='item'>
+  //     <img src={url + 'images/' + image} alt="kuva1" onDragStart={handleDragStart} role="presentation" />
+  //   </div>,
+  //   <div className='item'>
+  //     <img src={url + 'images/' + image} alt="kuva1" onDragStart={handleDragStart} role="presentation" />
+  //   </div>,
+  //   <div className='item'>
+  //     <img src={require("../images/kuva2.jpg")} alt="kuva2" onDragStart={handleDragStart} role="presentation" />
+  //   </div>,
+  //   <div className='item'>
+  //     <img src={require("../images/kuva3.jpg")} alt="kuva3" onDragStart={handleDragStart} role="presentation" />
+  //   </div>
+  // ];
 
   return (
       <div className='App'>
-        <AliceCarousel mouseTracking items={items} />
+        <AliceCarousel 
+        mouseTracking 
+        items={items} 
+        autoPlay={true}
+        autoPlayInterval={2000}
+        infinite={true}
+        paddingLeft={0}
+        paddingRight={0}
+        responsive={responsive}
+        disableButtonsControls={true}
+        controlsStrategy={"alternate"}
+        />
       </div>
   );
 }
