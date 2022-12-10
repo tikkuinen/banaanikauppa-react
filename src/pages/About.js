928babe27ab9a02cef9cf00420f2debb6b5be629
Saara87 <c1lasa04@students.oamk.fi>
2022-12-10 17:24:12 +0200

    About.css

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import './About.css'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+    <h1>Tietoa meistä</h1>
+      <h2>Verkkopalveluprojekti</h2>
+      <p className='teksti'>Riina Laukkanen, Tiia Lindell, Saara Laaninen ja Santeri Lassila TIK22KM </p>
+    </div>
   )
 }
