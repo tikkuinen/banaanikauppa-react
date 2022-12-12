5d4d3d73c93221ef11fd1a042607848931963dd1
tikkuinen <titunen@gmail.com>
2022-12-12 19:25:34 +0200

    karusellin korjaus

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,34 +7,12 @@ import './Carousel.css';
 
 export default function Carousel({url}) {
   const [product, setProduct] = useState([]);
-  const [rands, setRands] = useState([]);
-
-
-  //(url + 'products/getproducts.php/1')
-
-  // hakee taulukkona product-taulusta yhden, mut pitäiskö olla random kategoria?
-  const randomArray = (length, max) => 
-    Array(length).fill().map(() => Math.round(Math.random() * max));
 
   useEffect(() => {
     axios.get(url + 'products/getrandom.php')
       .then((response) => {
         const json = response.data;
         setProduct(json);
-        // tää nyt on muuten vaan kertomassa kuinka monta tuotetta sieltä tuli
-        console.log(json.length);
-        // tällä otetaan talteen se kuinka monta niitä tuli
-        const number = json.length;
-        
-        // luodaan taulukko, jossa arvotaan viisi lukua väliltä 0 - tuotteiden määrä
-        let randoooo = randomArray(10,number);
-        console.log(randoooo);
-        
-        // tässä nyt hakee sillä määritellyllä jutulla ollut
-        const randomized = json.filter( (item) => item.pub_year === "2000");
-        console.log(randomized);
-        setProduct(randomized);
-
       }).catch(error => {
         alert(error.response === undefined ? error : error.response.data.error);
       })
@@ -67,22 +45,6 @@ export default function Carousel({url}) {
       </div>
     </div>
   ));
-
-
-  // const items = [
-  //   <div className='item'>
-  //     <img src={url + 'images/' + image} alt="kuva1" onDragStart={handleDragStart} role="presentation" />
-  //   </div>,
-  //   <div className='item'>
-  //     <img src={url + 'images/' + image} alt="kuva1" onDragStart={handleDragStart} role="presentation" />
-  //   </div>,
-  //   <div className='item'>
-  //     <img src={require("../images/kuva2.jpg")} alt="kuva2" onDragStart={handleDragStart} role="presentation" />
-  //   </div>,
-  //   <div className='item'>
-  //     <img src={require("../images/kuva3.jpg")} alt="kuva3" onDragStart={handleDragStart} role="presentation" />
-  //   </div>
-  // ];
 
   return (
       <div className='App'>
